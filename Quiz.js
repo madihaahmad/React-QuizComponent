60edcfa846a7c716e082a7efc497f6b72c2d94ed
madihaahmad <39943341+madihaahmad@users.noreply.github.com>
2018-07-18 11:49:23 +0200

    Add files via upload

--- Quiz.js
+++ Quiz.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd'
 
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
   constructor(props){
     super(props)
-    this.state = {quiz_position:1}
+    this.state = {quiz_position:1,}
+    
 
   }
   render() {
+
+    const isQuizEnd = (this.state.quiz_position-1=== quizData.quiz_questions.length)?true:false
+    
+    if(isQuizEnd)
     return(
       <div>
-      <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]}/>
-
+      <QuizEnd />
+      
       </div>
     )
-  }
+      else
+      return(
+      <div>
+      <QuizQuestion
+      quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]}/>
+      </div>
+      )
+}
+
+    
+  
 }
 export default Quiz
